c0561bcdce0eb8e17dde6a3b8431178314b0538f
Lucas <123523359+lsbrel@users.noreply.github.com>
2024-01-31 09:02:03 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="d-flex flex-column gap-1 w-100 align-items-center">
+        <a href="./Gp/index.html">template - convencional</a>
+        <a href="./Remember/index.html">remember - template</a>
+        <a href="./Vlava/index.html">Vlava - template </a>
+        <a href="./UpConstruction/index.html">Up - template </a>
+        <a href="./chain/index.html">Chain - template </a>
+    </div>
+</body>
+</html>
